33cd1ea0ec4b9d1488e3564aa15e46808f1166e0
Mikelolpo10 <xzvmike@gmail.com>
2026-05-11 21:11:30 +0700

    Feature: added component to automatically scroll to the top after switching pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Routes, Route } from 'react-router'
 import Homepage from './pages/Homepage.jsx'
 import Legends from './pages/Legends.jsx'
+import ScrollToTop from './components/ScrollToTop.jsx'
 import './App.css'
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<Homepage />} />
-      <Route path='/legends' element={<Legends />} />
-      <Route path='/news/:id' />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path='/legends' element={<Legends />} />
+        <Route path='/news/:id' />
+      </Routes>
+    </>
   )
 }
 
