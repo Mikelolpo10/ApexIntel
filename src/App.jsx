fa379339a116856f2b7e58614020689e3f541d9f
Mikelolpo10 <xzvmike@gmail.com>
2026-05-12 16:25:05 +0700

    Feature: added path for legends page and error page if no path are matched

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router'
 import Homepage from './pages/Homepage.jsx'
 import LegendsMenu from './pages/LegendsMenu.jsx'
+import Legends from './pages/Legends.jsx'
+import ErrorPage from './pages/ErrorPage.jsx'
 import ScrollToTop from './components/ScrollToTop.jsx'
 import './App.css'
 
@@ -10,8 +12,10 @@ function App() {
       <ScrollToTop />
       <Routes>
         <Route index element={<Homepage />} />
-        <Route path='/legends' element={<LegendsMenu />} />
         <Route path='/news/:id' />
+        <Route path='/legends' element={<LegendsMenu />} />
+        <Route path='/legends/:name' element={<Legends />} />
+        <Route path='*' element={<ErrorPage />} />
       </Routes>
     </>
   )
